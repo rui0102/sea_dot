6d64d422100772413fb9b0bf8c51890e322f44fb
rui0102 <>
2020-11-13 23:08:31 +0900

    修正しました。バリデーションを追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
   mount_uploader :picture, PictureUploader 
-                          
+  validates :name, presence: true, length:{maximum: 30}
+  validates :age, length:{maximum: 3}
+  validates :ship_name, length:{maximum: 30}
+  validates :self_introduction, length:{maximum: 100}
+
 end
