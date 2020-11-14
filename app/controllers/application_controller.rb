2c8e5529f4332139bbdd83dec9c8e0ed012d5cf1
rui0102 <>
2020-11-14 11:51:49 +0900

    user/showアクションを編集しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
     creatures_infos_path
   end
 
+  # def after_sign_out_path_for(resource)
+  #   creatures_infos_path
+  # end
+
+  # def after_update_path_for(resource)
+  #   creatures_infos_path
+  # end
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,8 @@
 class UserController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
-  def show
-    @user = User.find(params[:id])
+  def show #編集必要
+    @user = current_user
   end
 
 end
