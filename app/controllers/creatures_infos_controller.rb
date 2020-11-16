90e47d3179d50da3207722e0deada8805e212c01
rui0102 <>
2020-11-16 19:16:05 +0900

    投稿機能実装。投稿詳細ページ編集。

--- app/controllers/creatures_infos_controller.rb
+++ app/controllers/creatures_infos_controller.rb
@@ -1,6 +1,7 @@
 class CreaturesInfosController < ApplicationController
-
+  
   def index
+    @creatures_info = CreaturesInfo.all
   end
 
   def new
@@ -8,13 +9,25 @@ class CreaturesInfosController < ApplicationController
   end
 
   def show
+    @creatures_info = CreaturesInfo.find(params[:id])
   end
 
   def create
+    @creatures_info = CreaturesInfo.new(creatures_info_params)
+    @creatures_info.user_id = current_user.id
+    @creatures_info.save
+    redirect_to creatures_info_path(@creatures_info)
   end
   
   def destroy
   end
+
+  private
+    def creatures_info_params
+      params.require(:creatures_info).permit(:content, :picture, :latitude,
+             :longitude, :weather, :sea_lavel, :visibility, :temperature, 
+             :departure, :destination)
+    end
   
   
 end
--- app/models/creatures_info.rb
+++ app/models/creatures_info.rb
@@ -1,5 +1,6 @@
 class CreaturesInfo < ApplicationRecord
-
+  belongs_to :user
+  mount_uploader :picture, PictureUploader 
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
   validates :latitude, presence: true, length: {in: 5..9}
@@ -7,11 +8,9 @@ class CreaturesInfo < ApplicationRecord
   validates :weather, length: {maximum: 5}
   validates :sea_lavel, length: {maximum: 2}
   validates :visibility, length: {maximum: 2}
-  validates :temperature, length: {maximum: 15}
+  validates :temperature, length: {maximum: 2}
   validates :departure, length: {maximum: 15}
-  
+  validates :destination, length: {maximum: 15}
 end
-
-
 
 
