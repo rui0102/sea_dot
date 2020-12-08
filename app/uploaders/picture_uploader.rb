3a1cbf76d191e82a5c6233b37f44fae11944e08a
rui0102 <>
2020-12-08 16:15:48 +0900

    本番環境での画像アップロードを調整しました。

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,7 +10,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
   #process :resize_to_fit => [220, 220]
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
