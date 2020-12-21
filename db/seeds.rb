f5ed3483419bf1394d31b21926b2323ec0354dad
田島瑞 <>
2020-12-21 17:16:49 +0900

    user1に画像を追加しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create!(
   name: '田島 るい',
-  #picture: '',
+  picture: File.open('./app/assets/images/user1.jpg'),
   age: '24',
   self_introduction: 'シルバーフェリーで三等航海士を努めています。',
   ship_name: 'シルバープリンセス',
